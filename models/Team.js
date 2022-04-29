f8ce8b22314ebeb4241fb207c7e448fe5f7a4430
arisrivaapple <arisrivaapple@brandeis.edu>
2022-04-29 17:39:55 -0400

    outline

---
+++ models/Team.js
@@ -0,0 +1,10 @@
+'use strict';
+const mongoose = require( 'mongoose' );
+const Schema = mongoose.Schema;
+
+var userSchema = Schema( {
+    :
+
+});
+
+module.exports = mongoose.model('User', userSchema);
